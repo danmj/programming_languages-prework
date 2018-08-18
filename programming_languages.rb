bb61e6a98ec078ce47579426ef1154e244595ec0
danmj <daniel.jung@stern.nyu.edu>
2018-08-18 15:35:49 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,22 +1,17 @@
 def reformat_languages(languages)
   # your code here
-    new_hash = {}
-  language_hash.each do |language_cat, languages|
-    languages.each do |language, attributes|
-      attributes.each do |attribute, att_value|
-        if language == :javascript
-          new_hash[language] = {
-            :type => att_value,
-            :style => [:oo, :functional]
+  new_hash = {}
+  language_hash.each do |language_category, languages|
+    languages.each do |language, characteristics|
+      characteristics.each do |characteristic, value|
+        new_hash[language_category] = {
+          language = {characteristics:value}
           }
-        else
-          new_hash[language] = {
-             :type => att_value,
-             :style => [language_cat]
-          }
+        if language_hash[:language_category] == :oo
+        elsif  language_hash[:language_category] == :functional
+        else puts "invalid option"
         end
       end
     end
   end
-  return new_hash
 end
